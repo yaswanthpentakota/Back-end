2a521c5619fc96b83c8affcc603f615fb13445bf
yaswanthpentakota <pentakotayaswanth11@gmail.com>
2025-12-05 10:07:50 +0530

    Implement async function to retrieve user details

---
+++ asyncawait.js
@@ -0,0 +1,18 @@
+const getUserDetails = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log({ roll: "23A91A0577" });
+            resolve("User details received");  
+        }, 2000);
+    });
+};
+
+const myfun = async () => {
+    console.log("marks");
+
+    const result = await getUserDetails("100");
+    console.log(result);  
+    console.log({ roll: "23A91A05F5" });
+};
+
+myfun();
